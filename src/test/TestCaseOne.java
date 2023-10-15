dd287667dbc45b11e31f33e57a777df8070223ae
Admin <Admin@DESKTOP-GS0CP1T>
2023-10-15 11:04:56 +0530

    Tanmay has uploaded the code...existing test case has been modified and new test case has been created.

--- src/test/TestCaseOne.java
+++ src/test/TestCaseOne.java
@@ -5,6 +5,7 @@ public class TestCaseOne {
 	public static void main(String[] args) {
 
 		System.out.println("This is Test Case One");
+		System.out.println("Hello");
 	}
 
 }
---
+++ src/test/TestCaseTwo.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TestCaseTwo {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is TestCase Two");
+	}
+
+}
